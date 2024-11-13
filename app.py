f6ae18e9bb2738786a4383a3beaf6f5b53069ad3
jdjose99 <jose.dhaga96@gmail.com>
2024-11-12 21:18:47 -0500

    updates

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import plotly_express as px
 import streamlit as st
 
 df = pd.read_csv("vehicles_us.csv")
+# creating a "manufacturer column"
 df["manufacturer"] = df["model"].apply(lambda x: x.split()[0])
 
+# replacing missing values with the median of each column
 columns_to_fill = ["model_year", "cylinders", "odometer", "is_4wd"]
 
+# getting rid of outliers
 for column in columns_to_fill:
     median_value = df[column].median()
     df[column].fillna(median_value, inplace=True)
@@ -23,20 +26,25 @@ df = removed_outliers
 st.header("Data viewer")
 st.dataframe(df)
 
+# first histogram
 st.header("Vehicle Types by Manufacturer")
 fig = px.histogram(df, x="manufacturer", color="type")
 st.write(fig)
 
+# second histogram
 st.header("Price vs Condition")
 fig = px.histogram(df, x="price", color="condition")
 st.write(fig)
 
+# third histogram
 st.header("Transmission Type by Manufacturer")
 fig = px.histogram(df, x="manufacturer", color="transmission")
 st.write(fig)
 
+# first scatter plot
 st.header("Price vs Year of Manufacture")
 
+# dropdown to select vehicle type
 vehicle_types = df["type"].unique()
 selected_type = st.selectbox("Select a Vehicle Type:", options=vehicle_types)
 
@@ -45,8 +53,10 @@ filtered_df = df[df["type"] == selected_type]
 fig = px.scatter(filtered_df, x="model_year", y="price", color="manufacturer")
 st.write(fig)
 
+# second scatter plot
 st.header("Relationship between Odometer & Year of Manufacture")
 
+# checkbox to remove odometer over 500k
 remove_high_odometer = st.checkbox("Remove vehicles with odometer over 500,000")
 
 if remove_high_odometer:
